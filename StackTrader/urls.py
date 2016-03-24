caea619b6b605616322c1783a4e862a42400deed
Kush Mansingh <kushmansingh@icloud.com>
2016-03-24 11:28:00 -0400

    Refactor view url routing

--- StackTrader/urls.py
+++ StackTrader/urls.py
@@ -5,11 +5,5 @@ from stapp import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('stapp.urls')),
-    url(r'^$', views.index, name='index'),
-    url(r'^signup/', views.signup, name='signup'),
-    url(r'^login/', views.login_user, name='login'),
-    url(r'^logout/', views.logout_user, name='logout'),
-    url(r'^stack/create/', views.create_stack, name='create'),
-    url(r'^stack/(?P<stack_name>\w+)/$', views.view_stack, name='view_stack')
+    url(r'^', include('stapp.urls'))
 ]
--- stapp/urls.py
+++ stapp/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^signup/', views.signup, name='signup'),
+    url(r'^login/', views.login_user, name='login'),
+    url(r'^logout/', views.logout_user, name='logout'),
+    url(r'^stack/create/', views.create_stack, name='create'),
+    url(r'^stack/(?P<stack_name>\w+)/$', views.view_stack, name='view_stack')
 ]
